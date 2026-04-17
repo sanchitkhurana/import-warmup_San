a2487406556c32c345d8a32200d1ab0f07894fb0
sanchitkhurana <sanchit.khurana94@gmail.com>
2026-04-17 16:03:41 +0000

    Exercise 2 ran successfully

--- run_exercises.py
+++ run_exercises.py
@@ -1,6 +1,7 @@
 
 import portfolio.data
 import portfolio.report
+import portfolio.metrics
 
 my_portfolio = portfolio.data.create_portfolio("Retirement")
 portfolio.report.print_report(my_portfolio)
